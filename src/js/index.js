9ac562bd291750548eb3a35d6d0a7d39b6a4dd46
ricwi1 <lunichgtgmail.com@MacBook-Air-LIUGAI.local>
2025-05-27 21:57:41 +0900

    scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
-import '../scss/style.scss'
+import '../scss/forDesktop/base.scss'
+import '../scss/forDesktop/aside.scss'
+import '../scss/forDesktop/mainSecondThird.scss'
+import '../scss/forDesktop/fourth.scss'
+import '../scss/forDesktop/footer.scss'
+
+import '../scss/for768/for768.scss'
+import '../scss/for360/for360.scss'
+
 import './script-expand.js';
 import './script-swiper.js';
 
